6ac06abad58557375cdbbd2f7063c0ef81437941
Folashy <100294216+Folashy@users.noreply.github.com>
2022-10-27 16:57:23 +0100

    added Social section style

--- src/components/SocialSection.js
+++ src/components/SocialSection.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import git from '../pictures/gitHub.svg';
 import slack from '../pictures/slack.svg'
+import { SocialSectionStyle } from './SocialSectionStyle';
 
 function SocialSection() {
   return (
-    <div>
+    <SocialSectionStyle>
      <img src={git} alt='gitHub'/>
      <img src={slack} alt='slack'/>
-    </div>
+    </SocialSectionStyle>
   )
 }
 
---
+++ src/components/SocialSectionStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const SocialSectionStyle = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 24px 0px;
+gap: 24px;
+width: 100%;
+height: 24px;
+`
